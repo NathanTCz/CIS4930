b0799660dff65b89dc19a6bc5756cc7c3132da48
NathanTcz <nathancazell@gmail.com>
2015-01-22 19:46:24 -0500

    functions to find certain weather data eg. max temp

--- assign2/weather_analyzer.py
+++ assign2/weather_analyzer.py
@@ -1,5 +1,33 @@
 from __future__ import print_function
 
+def find_max_temp(data):
+    max_temp = 0
+    for d in data:
+        if (d['TMAX'] > max_temp):
+            max_temp = d['TMAX']
+            day = d['DATE']
+    return max_temp, day
+
+def find_min_dif(data):
+    min_dif = float('inf')
+    for d in data:
+        if ( (float(d['TMAX']) - float(d['TMIN'])) < min_dif ):
+            min_dif = (float(d['TMAX']) - float(d['TMIN']))
+            day = d['DATE']
+    return min_dif, day
+
+def find_ave_wnd(data):
+    ave_wnd = 0
+    for d in data:
+        ave_wnd += float(d['AWND'])
+    return ( ave_wnd / len(data) )
+
+def tot_prcp(data):
+    tot_prcp = 0
+    for d in data:
+        tot_prcp += float(d['PRCP'])
+    return tot_prcp
+
 line_c = 0
 data = []
 with open(raw_input('File to be analyzed (.csv only): '), 'r') as file:
@@ -11,5 +39,4 @@ with open(raw_input('File to be analyzed (.csv only): '), 'r') as file:
         else:
             data.append( {k.strip('\n'): v.strip('\n') for k, v in zip(keys, line.split(','))} )
 
-for d in data:
-    print(d['TMAX'])
+print(tot_prcp(data))
