58a56837c0f441c0f8bb55e58cee6e8579ac20e4
NathanTcz <nathancazell@gmail.com>
2015-01-27 12:33:38 -0500

    ignore special characters in mixed_cipher

--- assign2/mixed_cipher.py
+++ assign2/mixed_cipher.py
@@ -15,12 +15,12 @@ keyword = rm_dups([x for x in raw_input('Please enter a keyword for the mixed ci
 cipher_alpha = rm_dups( keyword + lower_alpha )
 ciphered_file = []
 for char in characters:
-    if char is ' ' or char is '.':
-        ciphered_file.append(char)
-    elif char.islower():
+    if char.islower():
         ciphered_file.append( cipher_alpha[ lower_alpha.index(char) ] )
     elif char.isupper():
         ciphered_file.append( cipher_alpha[ lower_alpha.index( char.lower() ) ].upper() )
+    else:
+        ciphered_file.append(char)
 
 print('Plaintext:   ', end='')
 print(('').join(lower_alpha))
